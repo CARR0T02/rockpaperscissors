6861ac310489fb2680c66066ee1c3616a3f99054
CARR0T02 <mr.gabriel.chang@gmail.com>
2023-03-08 16:04:24 +0800

    Add JavaScript so that buttons can be used to play

--- JS.js
+++ JS.js
@@ -1,49 +1,79 @@
-let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
-    let choice;
-    let number = getRandomInt(3);
-    switch(number) {
-        case 0:
-            return choice = "rock";
-        case 1:
-            return choice = "paper";
-        case 2:
-            return choice = "scissors";
-    }
+  let number = getRandomInt(3);
+  switch (number) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
 }
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
 
-function playRound(playerSelection, computerSelection) {
-    let win = "You Win!";
-    let lose = "You Lose!";
-    let draw;
-    playerSelection = playerSelection.toLowerCase();
-    if (playerSelection != "paper" && playerSelection != "rock" & playerSelection != "scissors") {
-        return "ERROR WRONG INPUT";
-    }
-    if (playerSelection === computerSelection) {
-        return draw = "It's a draw";}
-    else if (playerSelection === "rock" && computerSelection === "paper" || playerSelection === "scissors" && computerSelection === "rock" || playerSelection === "paper" && computerSelection === "scissors" ) {
-        return lose = lose + ` ${computerSelection} beats ${playerSelection}`;} 
-    else {
-        return win = win + ` ${playerSelection} beats ${computerSelection}`;
-    }
+function playRound(e) {
+  const computerSelection = getComputerChoice();
+  let playerSelection = e.target.dataset.value;
+  let outcome = checkWinner(playerSelection, computerSelection);
+  updateScore(outcome);
+  console.log(
+    typeof playerSelection + typeof computerSelection + typeof outcome + outcome
+  );
+  if (outcome === "draw") {
+    roundOutcome.textContent = "It's a draw";
+  } else if (outcome === "lose") {
+    roundOutcome.textContent =
+      "You Lose!" + ` ${computerSelection} beats ${playerSelection}`;
+  } else {
+    roundOutcome.textContent =
+      "You Win!" + ` ${playerSelection} beats ${computerSelection}`;
+  }
 }
 
-function game() {
-    let count = 0;
-    for(let i = 0; i < 5; i++) {
-        playerSelection =  prompt("What would you like to choose?");
-        console.log(playerSelection);
-        if(playerSelection == undefined) {
-            break;   
-       }
-        console.log(playRound(playerSelection, getComputerChoice()));
-        console.log(++count);
-    }
-    console.log("end of game");
+function checkWinner(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "draw";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "scissors" && computerSelection === "rock") ||
+    (playerSelection === "paper" && computerSelection === "scissors")
+  ) {
+    return "lose";
+  }
+  return "win";
 }
+
+function updateScore(outcome) {
+  if (outcome === "win") {
+    playerScore += 1;
+  } else if (outcome === "lose") {
+    computerScore += 1;
+  }
+  if (playerScore === 5) {
+    runningScore.textContent = "Player wins the game!";
+    playerScore = 0;
+    computerScore = 0;
+  } else if (computerScore === 5) {
+    runningScore.textContent = "Computer wins the game!";
+    playerScore = 0;
+    computerScore = 0;
+  }
+  runningScore.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
+}
+
+function isValid(e) {
+  console.log(e.target.dataset.value);
+  console.log(e.target);
+  console.log(e);
+}
+
+const btns = document.querySelectorAll("button");
+btns.forEach((btn) => btn.addEventListener("click", playRound));
+const roundOutcome = document.body.appendChild(document.createElement("div"));
+const runningScore = document.body.appendChild(document.createElement("div"));
